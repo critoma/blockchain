7f3d60330e55152fe35f036b82cd7ee9ba96e747
critoma <cristianvtoma@gmail.com>
2024-06-16 21:22:53 +0300

    Create infuraWeb3_03_Compile_SmartContract.js

---
+++ ethereum/eth_web3js/infuraWeb3_03_Compile_SmartContract.js
@@ -0,0 +1,34 @@
+// 1. create account on Infura or Alchemy
+// 2. npm install web3@4.6.0
+// 3. npm i solc@0.8.25
+
+const fs = require("fs").promises;
+const solc = require("solc");
+
+async function main() {
+  // Load the contract source code
+  const sourceCode = await fs.readFile("Faucet.sol", "utf8");
+  // Compile the source code and retrieve the ABI and Bytecode
+  const { abi, bytecode } = compile(sourceCode, "Faucet");
+  // Store the ABI and Bytecode into a JSON file
+  const artifact = JSON.stringify({ abi, bytecode }, null, 2);
+  await fs.writeFile("Faucet.json", artifact);
+}
+
+function compile(sourceCode, contractName) {
+  // Create the Solidity Compiler Standard Input and Output JSON
+  const input = {
+    language: "Solidity",
+    sources: { main: { content: sourceCode } },
+    settings: { outputSelection: { "*": { "*": ["abi", "evm.bytecode"] } } },
+  };
+  // Parse the compiler output to retrieve the ABI and Bytecode
+  const output = solc.compile(JSON.stringify(input));
+  const artifact = JSON.parse(output).contracts.main[contractName];
+  return {
+    abi: artifact.abi,
+    bytecode: artifact.evm.bytecode.object,
+  };
+}
+
+main()
