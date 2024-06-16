9f1047a951564645be84f123f3eeff45fa7745ce
critoma <cristianvtoma@gmail.com>
2024-06-16 21:22:03 +0300

    Update Faucet.sol

--- ethereum/eth_web3js/Faucet.sol
+++ ethereum/eth_web3js/Faucet.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Faucet {
+    //constructor to set the owner
+        //constructor() payable {
+        //}
+
+    //function to donate funds to the faucet contract - the contract receive
+    receive () external payable {}
+
+    function withdraw(/*address payable _requestor,*/ uint /*calldata*/ withdraw_amount) public {
+        require(withdraw_amount <= 100000000000000000);
+        //msg.sender.transfer(withdraw_amount);
+        payable(msg.sender).transfer(withdraw_amount);
+        //_requestor.transfer(withdraw_amount);
+        //payable(msg.sender).send(withdraw_amount);
+    }
+}
