3bf190d6e35baf138355a098d29fcce45fce1b01
critoma <cristianvtoma@gmail.com>
2025-03-18 19:30:13 +0200

    Update server.js

--- ethereum/json-rpc-intro/server.js
+++ ethereum/json-rpc-intro/server.js
@@ -1,8 +1,8 @@
 // https://www.npmjs.com/package/json-rpc-2.0
 // npm install --save json-rpc-2.0 express
 /*
-secitc@cloudshell:~/blockchain/json-rpc$ npm ls
-json-rpc@ /home/secitc/blockchain/json-rpc
+ism_ase_ro@cloudshell:~/blockchain/json-rpc$ npm ls
+json-rpc@ /home/ism_ase_ro/blockchain/json-rpc
 ├── express@4.21.2
 └── json-rpc-2.0@1.7.0
 */
@@ -19,6 +19,21 @@ const server = new JSONRPCServer();
 // It can also return a promise of the result.
 server.addMethod("echo", ({ text }) => text);
 server.addMethod("log", ({ message }) => console.log(message));
+
+// Register the XOR function as a method in the server
+server.addMethod('xorArrays', ({arr1, arr2}) => {
+  // Ensure both arrays have the same length
+  if (arr1.length !== arr2.length) {
+    throw new Error('Arrays must have the same length');
+  }
+
+  // XOR operation on each corresponding pair of elements
+  // return arr1.map((value, index) => value ^ arr2[index]);
+  var arr = new Array(arr1.length);
+  for (var i = 0; i < arr.length; i++)
+      arr[i] = arr1[i] ^ arr2[i];
+  return arr;
+});
 
 const app = express();
 app.use(bodyParser.json());
